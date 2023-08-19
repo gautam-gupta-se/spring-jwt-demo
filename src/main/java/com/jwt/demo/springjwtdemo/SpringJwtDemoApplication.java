cadf12f69e45b11f79cf43dc2e895706ac213d9b
gautam gupta <gg300906@gmail.com>
2023-08-19 23:43:12 +0530

    first commit

---
+++ src/main/java/com/jwt/demo/springjwtdemo/SpringJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jwt.demo.springjwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJwtDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwt/demo/springjwtdemo/SpringJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt.demo.springjwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
